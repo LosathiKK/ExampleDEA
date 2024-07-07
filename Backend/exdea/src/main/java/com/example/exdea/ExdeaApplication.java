761d3a913d1c245eeaaaa6b16c0680b99c3b6904
Sanali Losathi <kklosathi19@gmail.com>
2024-07-08 01:07:34 +0530

    added backend file

---
+++ Backend/exdea/src/main/java/com/example/exdea/ExdeaApplication.java
@@ -0,0 +1,13 @@
+package com.example.exdea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExdeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExdeaApplication.class, args);
+	}
+
+}
